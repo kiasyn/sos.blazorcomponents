3de5dfb57cd5ae6f9d5886c653efd86112be0e37
Sam O'Connor <sam@kiasyn.com>
2020-04-25 16:01:22 +1200

    Import code

---
+++ DatePickerBase.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Sos.BlazorComponents
+{
+    public enum DatePickerView
+    {
+        Days,
+        Months,
+        Decades,
+    }
+    public class DatePickerBase : InputBase<DateTime?>
+    {
+        [Parameter]
+        public DatePickerView View { get; set; } = DatePickerView.Days;
+        [Parameter]
+        public DayOfWeek StartDayOfWeek { get; set; } = DayOfWeek.Wednesday;
+        [Parameter]
+        public DateTime? StartDate { get; set; }
+        [Parameter]
+        public DateTime? EndDate { get; set; }
+        [Parameter]
+        public DateTime ViewDate { get; set; } = DateTime.UtcNow;
+        [Parameter]
+        public string DateFormat { get; set; } = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
+        [Parameter]
+        public bool CloseOnSelect { get; set; } = true;
+        [Parameter]
+        public Func<DateTime, bool> DateDisabledCallback { get; set; }
+
+        private string _inputValue;
+        public string InputValue
+        {
+            get => _inputValue; set
+            {
+                _inputValue = value;
+                InputValueChanged();
+            }
+        }
+
+        public IEnumerable<DayOfWeek> WeekDays => GetWeekDays(StartDayOfWeek).ToList();
+        public DateTime StartOfMonth => ViewDate.StartOfMonth();
+        public DateTime EndOfMonth => ViewDate.EndOfMonth();
+        public DateTime DayViewStartDate => StartOfMonth.StartOfWeek(this.StartDayOfWeek);
+        public DateTime DayViewEndDate => GetDayViewEndDate();
+
+        private DateTime GetDayViewEndDate()
+        {
+            var dayViewEndDate = EndOfMonth.EndOfWeek((DayOfWeek)(((int)this.StartDayOfWeek + 7) % 7));
+            var weeks = (dayViewEndDate - DayViewStartDate).TotalDays / 7;
+            dayViewEndDate = dayViewEndDate.AddDays((6 - weeks) * 7);
+            return dayViewEndDate;
+        }
+
+        public DateTime MonthViewStartDate => ViewDate.StartOfYear();
+        public DateTime MonthViewEndDate => ViewDate.EndOfYear();
+        public DateTime StartOfDecade => ViewDate.StartOfDecade();
+        public DateTime EndOfDecade => ViewDate.EndOfDecade();
+        public DateTime DecadeViewStartDate => StartOfDecade.SafeAddYears(-1);
+        public DateTime DecadeViewEndDate => EndOfDecade.SafeAddYears(1);
+        public DateTime StartOfCentury => ViewDate.StartOfCentury();
+        public DateTime EndOfCentury => ViewDate.EndOfCentury();
+        public DateTime StartOfMillenium => ViewDate.StartOfMillenium();
+        public DateTime EndOfMillenium => ViewDate.EndOfMillenium();
+        public DateTime CenturyViewStartDate => StartOfMillenium.SafeAddYears(-100);
+        public DateTime CenturyViewEndDate => EndOfMillenium.SafeAddYears(100);
+        public bool Expanded { get; set; }
+
+        protected override void OnInitialized()
+        {
+            this.InputValue = Value?.ToString(DateFormat);
+        }
+
+        protected override bool TryParseValueFromString(string value, out DateTime? result, out string validationErrorMessage)
+        {
+            Console.WriteLine($"TryParseValueFromString: {value}");
+            validationErrorMessage = null;
+            var parsed = DateTime.TryParseExact(value, DateFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out var dt);
+            result = parsed ? (DateTime?)dt : null;
+            return parsed;
+        }
+
+        protected override string FormatValueAsString(DateTime? value)
+        {
+            return value?.ToString(DateFormat);
+        }
+
+        protected void Next()
+        {
+            ViewDate = this.View switch
+            {
+                DatePickerView.Decades => ViewDate.SafeAddYears(100),
+                DatePickerView.Months => ViewDate.SafeAddYears(1),
+                DatePickerView.Days => ViewDate.AddMonths(1),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        protected void Previous()
+        {
+            ViewDate = this.View switch
+            {
+                DatePickerView.Decades => ViewDate.SafeAddYears(-100),
+                DatePickerView.Months => ViewDate.SafeAddYears(-1),
+                DatePickerView.Days => ViewDate.AddMonths(-1),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        protected IEnumerable<DayOfWeek> GetWeekDays(DayOfWeek startDayOfWeek)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                var dow = (DayOfWeek)(((int)startDayOfWeek + i) % 7);
+                yield return dow;
+            }
+        }
+
+        protected bool IsDateDisabled(DateTime dt)
+        {
+            return (StartDate != null && dt < StartDate)
+                || (EndDate != null && dt > EndDate)
+                || (DateDisabledCallback?.Invoke(dt) == true);
+        }
+
+        protected void SetDate(DateTime dt)
+        {
+            if (IsDateDisabled(dt))
+            {
+                return;
+            }
+
+            this.Value = dt;
+            this.InputValue = dt.ToString(DateFormat);
+            if (CloseOnSelect)
+            {
+                this.Expanded = false;
+            }
+        }
+
+        protected void SetView(DatePickerView view, int? year = null, int? month = null, int? day = null)
+        {
+            this.View = view;
+            this.ViewDate = new DateTime(year ?? ViewDate.Year, month ?? ViewDate.Month, day ?? ViewDate.Day);
+        }
+
+        protected void InputValueChanged()
+        {
+            if (DateTime.TryParseExact(InputValue, DateFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out var dt))
+            {
+                this.Value = dt;
+            }
+        }
+
+        protected void InputValueBlur()
+        {
+            this.InputValue = this.Value?.ToString(DateFormat);
+        }
+    }
+}
---
+++ DateTimeUtilities.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Sos.BlazorComponents
+{
+    internal static class DateTimeUtilities
+    {
+        public static DateTime StartOfMonth(this DateTime dt)
+        {
+            return CreateRepresentableDateTime(dt.Year, dt.Month, 1);
+        }
+
+        public static DateTime EndOfMonth(this DateTime dt)
+        {
+            return CreateRepresentableDateTime(dt.Year, dt.Month, DateTime.DaysInMonth(dt.Year, dt.Month));
+        }
+
+        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+        {
+            int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+            return dt.AddDays(-1 * diff).Date;
+        }
+
+        public static DateTime EndOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+        {
+            int diff = (7 - (dt.DayOfWeek - startOfWeek)) % 7;
+            return dt.AddDays(1 * diff).Date;
+        }
+
+        public static DateTime StartOfYear(this DateTime dt)
+        {
+            return CreateRepresentableDateTime(dt.Year, 1, 1);
+        }
+
+        public static DateTime EndOfYear(this DateTime dt)
+        {
+            return CreateRepresentableDateTime(dt.Year + 1, 1, 1).AddTicks(-1);
+        }
+
+        public static DateTime StartOfDecade(this DateTime dt)
+        {
+            return CreateRepresentableDateTime((int)(dt.Year / 10) * 10, 1, 1);
+        }
+
+        public static DateTime EndOfDecade(this DateTime dt)
+        {
+            return CreateRepresentableDateTime(((int)(dt.Year / 10) * 10) + 10, 1, 1).AddTicks(-1);
+        }
+
+        public static DateTime StartOfCentury(this DateTime dt)
+        {
+            return CreateRepresentableDateTime((int)(dt.Year / 100) * 100, 1, 1);
+        }
+
+        public static DateTime EndOfCentury(this DateTime dt)
+        {
+            return CreateRepresentableDateTime(((int)(dt.Year / 100) * 100) + 100, 1, 1).AddTicks(-1);
+        }
+
+        public static DateTime StartOfMillenium(this DateTime dt)
+        {
+            return CreateRepresentableDateTime((int)(dt.Year / 1000) * 1000, 1, 1);
+        }
+
+        public static DateTime EndOfMillenium(this DateTime dt)
+        {
+            return CreateRepresentableDateTime(((int)(dt.Year / 1000) * 1000) + 1000, 1, 1).AddTicks(-1);
+        }
+
+        public static DateTime SafeAddYears(this DateTime dt, int years)
+        {
+            int year = dt.Year + years;
+            return CreateRepresentableDateTime(year, dt.Month, dt.Day);
+        }
+
+        private static DateTime CreateRepresentableDateTime(int year, int month, int day)
+        {
+            if (year <= 0)
+            {
+                year = 1;
+            }
+            if (year >= 10000)
+            {
+                year = 9999;
+            }
+            return new DateTime(year, month, day);
+        }
+    }
+}
